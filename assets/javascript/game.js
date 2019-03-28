5e0fa6053c0e4b6f82b6852671c3d82030653b75
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-03-28 14:45:23 -0400

    completed acceptance and dislay of correct letter input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,88 +1,4 @@
 
-function withWords() {
-// alphabet
-    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Words to guess and hints.
-    var word = {
-        "dwarfs" : {
-            type: "Race.", 
-            desc: "Short, enjoys mining."
-        },
-        "knight" : {
-            type: "Soldier.", 
-            desc: "Follows code of chilvary. Wears heavy armor."
-        },
-        "elves" : {
-            type: "Race.", 
-            desc: "Tall and lean. Pointy ears."
-        },
-        "mage" : {
-            type: "Soldier.", 
-            desc: "Performs magic."
-        },
-        "skaven" : {
-            type: "Race.",
-            desc: "Small and rat-like."
-        },
-        "greenskins" : {
-            type: "Race.",
-            desc: "Burly and pugnacious. Think orc."
-        },
-        "vampires" : {
-            type: "Race.",
-            desc: "Likes blood. May or may not have died at some point."
-        },
-        "axe" : {
-            type: "Weapon.", 
-            desc: "Sharp and choppy."
-        },
-        "sword" : {
-            type: "Weapon.",
-            desc: "Long and pointy. Stabby and slashy."
-        },
-        "mace" : {
-            type: "Weapon.",
-            desc: "Blunt and heavy. Smashy like a hammer."},
-        "cavalry" : {
-            type: "Soldier.",
-            desc: "Rides a steed."},
-        };
-
-    var wordArray = ["dwarfs", "knight", "elves", "mage", "skaven", "greenskins", "vampires", "axe", "sword", "mace", "cavalry"];
-
-    // RNG to pull from array
-    var wordArrayRNG = Math.floor((Math.random() * 10) + 0);
-    console.log("Random number generator created = " + wordArrayRNG);
-    // Verify the wordArray[wordArrayRNG] functions
-    var wordProperty = wordArray[wordArrayRNG];
-    console.log("Word to guess = " + wordProperty);
-
-    // Set up display array.
-        // Create variable with array length
-    var arrayLength = wordProperty.length;
-    console.log("Expected word length = " + arrayLength);
-        // Create array to fill
-    var blankArray = [];
-        // Fill with appropriate number of blanks
-    for (i = 0; i < arrayLength; i++) {
-        blankArray.push("_ ");
-    };
-
-    console.log("Word line holds: " + blankArray.join(""));
-    document.getElementById("word").innerHTML = blankArray.join("");
-    // console.log(word[wordArray[wordArrayRNG]].type);
-    document.getElementById("clue2").innerHTML = word[wordArray[wordArrayRNG]].type + "<br>" +  word[wordArray[wordArrayRNG]].desc;
-    // Begin logic to capture keypress event for guess and store/display.
-    document.onkeyup = function(event) {
-        var keyPress = event.key.toLowerCase;
-        var keyTruth = alphabet.includes(keyPress);
-        if (keyTruth === true) {
-            var wordSplit = wordProperty.split();
-        }
-    }
-    
-}
-
 function displayUp() {
     document.getElementById("clue1").innerHTML = "Clue:";
     document.getElementById("clue2").innerHTML = "";
@@ -90,7 +6,7 @@ function displayUp() {
     document.getElementById("let-guess").innerHTML = "";
     document.getElementById("guess-rem").innerHTML = "";
 
-    withWords();
+    // withWords();
 }
 
 function startGame() {
@@ -114,4 +30,101 @@ function startGame() {
 startGame();
 
 
-// document.getElementById("clue").innerHTML = "";
+// function withWords() {
+    // alphabet
+        var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    // Words to guess and hints.
+        var word = {
+            "dwarfs" : {
+                type: "Race.", 
+                desc: "Short, enjoys mining."
+            },
+            "knight" : {
+                type: "Soldier.", 
+                desc: "Follows code of chilvary. Wears heavy armor."
+            },
+            "elves" : {
+                type: "Race.", 
+                desc: "Tall and lean. Pointy ears."
+            },
+            "mage" : {
+                type: "Soldier.", 
+                desc: "Performs magic."
+            },
+            "skaven" : {
+                type: "Race.",
+                desc: "Small and rat-like."
+            },
+            "greenskins" : {
+                type: "Race.",
+                desc: "Burly and pugnacious. Think orc."
+            },
+            "vampires" : {
+                type: "Race.",
+                desc: "Likes blood. May or may not have died at some point."
+            },
+            "axe" : {
+                type: "Weapon.", 
+                desc: "Sharp and choppy."
+            },
+            "sword" : {
+                type: "Weapon.",
+                desc: "Long and pointy. Stabby and slashy."
+            },
+            "mace" : {
+                type: "Weapon.",
+                desc: "Blunt and heavy. Smashy like a hammer."},
+            "cavalry" : {
+                type: "Soldier.",
+                desc: "Rides a steed."},
+            };
+    
+        var wordArray = ["dwarfs", "knight", "elves", "mage", "skaven", "greenskins", "vampires", "axe", "sword", "mace", "cavalry"];
+    
+        // RNG to pull from array
+        var wordArrayRNG = Math.floor((Math.random() * 10) + 0);
+        console.log("Random number generator created = " + wordArrayRNG);
+        // Verify the wordArray[wordArrayRNG] functions
+        var wordProperty = wordArray[wordArrayRNG];
+        console.log("Word to guess = " + wordProperty);
+    
+        // Set up display array.
+            // Create variable with array length
+        var arrayLength = wordProperty.length;
+        console.log("Expected word length = " + arrayLength);
+            // Create array to fill
+        var blankArray = [];
+            // Fill with appropriate number of blanks
+        for (i = 0; i < arrayLength; i++) {
+            blankArray.push("_ ");
+        };
+    
+        console.log("Word line holds: " + blankArray.join(""));
+        document.getElementById("word").innerHTML = blankArray.join("");
+        // console.log(word[wordArray[wordArrayRNG]].type);
+        document.getElementById("clue2").innerHTML = word[wordArray[wordArrayRNG]].type + "<br>" +  word[wordArray[wordArrayRNG]].desc;
+        // Begin logic to capture keypress event for guess and store/display.
+        document.onkeyup = function(event) {
+            console.log(event.key);
+            var keyPress = event.key.toLowerCase();
+            console.log(keyPress);
+            var keyTruth = alphabet.includes(keyPress);
+            console.log(keyTruth);
+            if (keyTruth === true) {
+                var wordSplit = wordProperty.split("");
+                console.log(wordProperty);
+                console.log("word split contains: " + wordSplit)
+                for (var i = 0; i < wordSplit.length; i++)
+                    if (wordSplit[i] == keyPress) {
+                        blankArray[i] = keyPress;
+                        console.log("blank array conains: " + blankArray);
+                        document.getElementById("word").innerHTML = blankArray.join("");
+                    }
+                    else {
+                        
+                    }
+            }
+        }
+        
+    // }
+    
